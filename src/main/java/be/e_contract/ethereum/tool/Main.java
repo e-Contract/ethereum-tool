2ab8c46c4e821b3d0397cdffc6cd016971559775
Frank Cornelis <info@e-contract.be>
2018-03-08 17:58:54 +0100

    initial project structure

---
+++ src/main/java/be/e_contract/ethereum/tool/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Ethereum Tool project.
+ *
+ * Copyright 2018 e-Contract.be BVBA. All rights reserved.
+ * e-Contract.be BVBA proprietary/confidential. Use is subject to license terms.
+ */
+package be.e_contract.ethereum.tool;
+
+/**
+ * Ethereum Tool main class.
+ *
+ * @author Frank Cornelis
+ */
+public class Main {
+
+}
