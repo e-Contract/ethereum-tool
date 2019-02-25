f13e32937773794fd865de3f614267015d103153
Frank Cornelis <info@e-contract.be>
2019-02-25 03:37:14 +0100

    historical balance option

--- src/main/java/be/e_contract/ethereum/tool/Balance.java
+++ src/main/java/be/e_contract/ethereum/tool/Balance.java
@@ -1,6 +1,6 @@
 /*
  * Ethereum Tool project.
- * Copyright (C) 2018 e-Contract.be BVBA.
+ * Copyright (C) 2018-2019 e-Contract.be BVBA.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version
@@ -27,7 +27,9 @@ import okhttp3.Request;
 import okhttp3.Response;
 import org.web3j.crypto.Keys;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 import org.web3j.utils.Convert;
 import picocli.CommandLine;
 
@@ -40,14 +42,30 @@ public class Balance implements Callable<Void> {
     @CommandLine.Option(names = {"-a", "--address"}, required = true, description = "the key address")
     private Address address;
 
+    @CommandLine.Option(names = {"-n", "--number"}, description = "the block number")
+    private BigInteger blockNumber;
+
     @Override
     public Void call() throws Exception {
         System.out.println("Address: " + this.address.getAddress());
         String checksumAddress = Keys.toChecksumAddress(this.address.getAddress());
         System.out.println("Address (checksum): " + checksumAddress);
-        BigInteger transactionCount = this.web3.ethGetTransactionCount(this.address.getAddress(), DefaultBlockParameterName.LATEST).send().getTransactionCount();
+        BigInteger balanceBlockNumber;
+        if (null != this.blockNumber) {
+            balanceBlockNumber = this.blockNumber;
+        } else {
+            balanceBlockNumber = this.web3.ethBlockNumber().send().getBlockNumber();
+        }
+        System.out.println("Block number: " + balanceBlockNumber);
+        EthGetTransactionCount ethGetTransactionCount = this.web3.ethGetTransactionCount(this.address.getAddress(), DefaultBlockParameter.valueOf(balanceBlockNumber)).send();
+        if (ethGetTransactionCount.hasError()) {
+            Output.error("Could not retrieve transaction count for block " + balanceBlockNumber);
+            Output.error(ethGetTransactionCount.getError().getMessage());
+            return null;
+        }
+        BigInteger transactionCount = ethGetTransactionCount.getTransactionCount();
         System.out.println("Transaction count: " + transactionCount);
-        BigInteger balance = this.web3.ethGetBalance(this.address.getAddress(), DefaultBlockParameterName.LATEST).send().getBalance();
+        BigInteger balance = this.web3.ethGetBalance(this.address.getAddress(), DefaultBlockParameter.valueOf(balanceBlockNumber)).send().getBalance();
         BigDecimal balanceEther = Convert.fromWei(new BigDecimal(balance), Convert.Unit.ETHER);
         System.out.println("Balance: " + balanceEther + " ether");
 
